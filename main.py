43866fe747e862d9e9180b7227f9a55c2c8902a0
dotXem <maxime.debois@gmail.com>
2019-06-27 17:19:03 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+from cg_ega import CG_EGA
+
+
+def main():
+    y_true, y_pred = np.load("example.npy")
+    freq = 5
+
+    cg_ega = CG_EGA(y_true, y_pred, freq)
+    cg_ega.plot(day=0)
+
+if __name__ == "__main__":
+    main()

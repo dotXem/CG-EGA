767ccdedab933931887ce236990258072e9f7bfd
dotXem <maxime.debois@gmail.com>
2019-09-24 16:11:17 +0200

    removed tools folder

--- tools/misc.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def _any(l, axis=1):
-    return np.reshape(np.any(np.concatenate(l, axis=axis), axis=axis), (-1, 1))
-
-
-def _all(l, axis=1):
-    return np.reshape(np.all(np.concatenate(l, axis=axis), axis=axis), (-1, 1))
